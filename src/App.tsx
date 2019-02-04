6b32344a2d80a7dba819400175c48d9b5eeca0be
Sean Smith <sean.smith@tenx.tech>
2019-02-04 10:47:12 +0800

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,29 @@ import * as React from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
+  async componentDidMount() {
+    try {
+      const data = {
+        message: "Hello from Earth",
+        author: {
+          name: "Seanie X",
+        },
+      };
+
+      const result = await fetch("http://localhost:8000/post_message", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+      const response = await result.json();
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }
   render() {
     return (
       <View style={styles.container}>

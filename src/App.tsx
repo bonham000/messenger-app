c2df29492e85f31e99d5c54fac747ce3cecaad65
Sean Smith <sean.smith@tenx.tech>
2019-02-09 18:32:29 +0800

    Revert...

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,6 @@ import {
 } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
-// TEST PR
-
 /** ******************************************************************************
  * Config
  * ********************************************************************************
